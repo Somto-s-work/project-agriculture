6c8921ea42c483009297aa66e17e91148f729f3d
ozosomto <143817351+ozosomto@users.noreply.github.com>
2024-03-04 12:52:15 +0100

    Add files via upload

---
+++ Project agriculture/assets/js/index.js
@@ -0,0 +1,10 @@
+
+
+
+let added_heading = document.getElementById("added-heading")
+console.log(added_heading)
+
+added_heading.addEventListener("click", (e) =>{
+added_heading.style.width = "100%"
+
+} )
